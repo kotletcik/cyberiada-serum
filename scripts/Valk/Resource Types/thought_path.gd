375a54a13c4fdc5377d36ef1e2a48185e0597810
Valk0x29a <62944006+valk0x29a@users.noreply.github.com>
2026-03-03 12:30:59 +0100

    meow meow

--- scripts/Valk/Resource Types/thought_path.gd
+++ scripts/Valk/Resource Types/thought_path.gd
@@ -5,3 +5,7 @@ extends Resource
 @export var required_clues: Array[Clue] = [null];
 @export var is_clue_realized: Array[bool] = [false];
 @export var does_automatically_unlock: Array[bool] = [false];
+
+func is_unlocked() -> bool:
+    if(required_clues.size() <= 0 || is_clue_realized.size() <= 0): return false;
+    return is_clue_realized[0];
