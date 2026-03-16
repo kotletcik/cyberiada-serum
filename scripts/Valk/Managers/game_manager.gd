06e7d807c2288e89e9f8439b02ebc0e2aa72988f
Valk0x29a <62944006+valk0x29a@users.noreply.github.com>
2026-03-16 12:32:05 +0100

    game over + pause on esc menu

--- scripts/Valk/Managers/game_manager.gd
+++ scripts/Valk/Managers/game_manager.gd
@@ -4,14 +4,30 @@ extends Node
 static var instance: GameManager;
 @onready var player: PlayerController = $"../Player" #Link na playera w globalnej scenie
 
+var is_game_over: bool = false;
+
 func _ready() -> void:
 	if(instance == null):
 		instance = self;    
+		is_game_over = false;
 	else:
 		print("More than one GameManager exists!!!");
 		queue_free();
 		
 
+func game_over() -> void:
+	is_game_over = true;
+	pause_game();
+	UIManager.instance.show_game_over();
+
 func restart_scene() -> void:
+	is_game_over = false;
 	get_tree().get_root().request_ready();
 	get_tree().reload_current_scene();
+	# get_tree().paused = false;
+
+func pause_game() -> void:
+	get_tree().paused = true;
+
+func unpause_game() -> void:
+	get_tree().paused = false;
--- scripts/Valk/root_node_script.gd
+++ scripts/Valk/root_node_script.gd
@@ -3,4 +3,5 @@ extends Node3D
 func _ready() -> void:
 	print("ready works");
 	SaveManager.instance.load_last_checkpoint();
+	get_tree().paused = false;
 		
