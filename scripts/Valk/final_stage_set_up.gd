8b683a068f1ea20b4fc95cf6d81bba2bf89ef4e0
Valk0x29a <62944006+valk0x29a@users.noreply.github.com>
2026-05-17 13:25:47 +0200

    meow

--- scripts/Valk/final_stage_set_up.gd
+++ scripts/Valk/final_stage_set_up.gd
@@ -1,19 +1,19 @@
 extends Node
 
 @export var shells: Array[Shell_behaviour] = [null];
-@export var shell_body: Node3D
-@export var lift_shell: Shell_behaviour
-@export var ending_trigger: EventTrigger
+# @export var shell_body: Node3D
+# @export var lift_shell: Shell_behaviour
+# @export var ending_trigger: EventTrigger
 
 func _ready() -> void:
 	EventBus.final_stage.connect(reappear_shells);
-	if(shell_body != null): shell_body.visible = false;
-	ending_trigger.enable_trigger = false;
+	# if(shell_body != null): shell_body.visible = false;
+	# ending_trigger.enable_trigger = false;
 
 func reappear_shells():
-	if(shell_body != null): shell_body.visible = true;
-	lift_shell.disable();
-	ending_trigger.enable_trigger = true;
+	# if(shell_body != null): shell_body.visible = true;
+	# lift_shell.disable();
+	# ending_trigger.enable_trigger = true;
 	for i in range(0, shells.size()):
 		if(shells[i] == null): continue;
 		shells[i].enable();
